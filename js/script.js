02983384e82c8a1e08a396ad2d94528427736ab1
Hikikamaru <gayzzz228@gmail.com>
2020-09-24 14:34:57 +0300

    Added Rest operator that serves as insurance if we want to expand the section with cards (modifications)

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     //Timer
-  
+
     const deadline = '2020-09-28';
 
     function getTimeRamaining(endtime) {
@@ -47,13 +47,13 @@ window.addEventListener('DOMContentLoaded', () => {
             hours = Math.floor((t / (1000 * 60 * 60) % 24)), //Отримаємо скільки годин залишилось до кінця акції, %24 це той хвостик якого не вистачає до 24 годин і він буде повертатися в години тобто якщо до кінця акції 50 годин ми ділимо на 24 отримаємо цілих два дні  а 2 години підуть до годин в таймері 
             minutes = Math.floor((t / 1000 / 60) % 60),
             seconds = Math.floor((t / 1000) % 60);
-            return {
-                'total' : t,
-                'hours' : hours,
-                'minutes' : minutes,
-                'seconds' : seconds,
-                'days': days
-            };
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+            'days': days
+        };
 
     }
 
@@ -78,17 +78,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
         function updateClock() { //Ця функція буде виводити наш час на сторінку
             const t = getTimeRamaining(endtime)
-                days.innerHTML = getZero(t.days),
+            days.innerHTML = getZero(t.days),
                 hours.innerHTML = getZero(t.hours),
                 minutes.innerHTML = getZero(t.minutes),
                 seconds.innerHTML = getZero(t.seconds);
 
 
 
-            
-            if (t.total <= 0) {//Цей фунціонал зупиняє таймер коли внашому t закінчуються мілісекунди для цього ми його і рахували
+
+            if (t.total <= 0) { //Цей фунціонал зупиняє таймер коли внашому t закінчуються мілісекунди для цього ми його і рахували
                 clearInterval(timeInterval);
-                
+
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
@@ -148,26 +148,34 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
     window.addEventListener('scroll', showModalByScroll); // Показує модальне вікно коли користувач доскролив до кінця сторінки
-     //Patterning (Шаблонізація)
-    
-     class MenuCard{
-         constructor(src,alt,title,descr,price,parentSelector){
-             this.src=src;
-             this.alt=alt;
-             this.title=title;
-             this.descr=descr;
-             this.price=price;
-             this.parent=document.querySelector(parentSelector);
-             this.transfer=27;
-             this.changeToUAH();
-         }
-changeToUAH(){
-    this.price=this.price*this.transfer
-}
-render(){
-     const element = document.createElement('div');
-     element.innerHTML=`
-     <div class="menu__item">
+    //Patterning (Шаблонізація)
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes,
+                this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer
+        }
+        render() {
+            const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(classesName => element.classList.add(classesName))
+            }
+
+            element.innerHTML = `
+     
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -176,39 +184,40 @@ render(){
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
+                
      `;
-     this.parent.append(element);
-}
+            this.parent.append(element);
         }
-        new MenuCard(
-            "img/tabs/vegy.jpg",
-            "vegy",
-             'Меню "Фитнес"',
-             'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-             9,
-             '.menu .container'
-            
-        ).render();
-
-        new MenuCard(
-            "img/tabs/elite.jpg",
-            "elite",
-             'Меню “Премиум”',
-             'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-             14,
-             '.menu .container'
-            
-        ).render();
+    }
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
         
-        new MenuCard(
-            "img/tabs/post.jpg",
-            "post",
-             'Меню "Постное"',
-             'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-             21,
-             '.menu .container'
-            
-        ).render();
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container',
        
-}); 
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container',
+        
+    ).render();
+
+});
